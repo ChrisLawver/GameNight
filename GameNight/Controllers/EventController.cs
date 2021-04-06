af8b7559cadd08a639a72378bb33d9d4c7936af7
Adam Pryatel <adampryatel@gmail.com>
2021-04-06 14:44:32 -0400

    Added Controllers

---
+++ GameNight/Controllers/EventController.cs
@@ -0,0 +1,65 @@
+﻿using GameNight.Models;
+using GameNight.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameNight.Controllers
+{
+    public class EventController : Controller
+    {
+
+        IRepository<Event> eventRepo;
+
+        public EventController(IRepository<Event> eventRepo)
+        {
+            this.eventRepo = eventRepo;
+        }
+
+        public ViewResult Index()
+        {
+            var eventList = eventRepo.GetAll();
+            return View(eventList);
+        }
+
+        public ViewResult Details(int id)
+        {
+            var event1 = eventRepo.GetById(id);
+            return View(event1);
+        }
+
+        public ViewResult Create()
+        {
+            return View(new Event());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Event model)
+        {
+            eventRepo.Create(model);
+            return View(model);
+        }
+
+        public ViewResult Update(int id)
+        {
+            var event1 = eventRepo.GetById(id);
+            return View(event1);
+        }
+
+        [HttpPost]
+        public ActionResult Update(Event model)
+        {
+            eventRepo.Update(model);
+            return View(model);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var event1 = eventRepo.GetById(id);
+            eventRepo.Delete(event1);
+            return RedirectToAction("Event");
+        }
+    }
+}
---
+++ GameNight/Controllers/GameController.cs
@@ -0,0 +1,65 @@
+﻿using GameNight.Models;
+using GameNight.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameNight.Controllers
+{
+    public class GameController : Controller
+    {
+
+        IRepository<Game> gameRepo;
+
+        public GameController(IRepository<Game> gameRepo)
+        {
+            this.gameRepo = gameRepo;
+        }
+
+        public ViewResult Index()
+        {
+            var gameList = gameRepo.GetAll();
+            return View(gameList);
+        }
+
+        public ViewResult Details(int id)
+        {
+            var game = gameRepo.GetById(id);
+            return View(game);
+        }
+
+        public ViewResult Create()
+        {
+            return View(new Game());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Game model)
+        {
+            gameRepo.Create(model);
+            return View(model);
+        }
+
+        public ViewResult Update(int id)
+        {
+            var game = gameRepo.GetById(id);
+            return View(game);
+        }
+
+        [HttpPost]
+        public ActionResult Update(Game model)
+        {
+            gameRepo.Update(model);
+            return View(model);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var game = gameRepo.GetById(id);
+            gameRepo.Delete(game);
+            return RedirectToAction("Game");
+        }
+    }
+}
